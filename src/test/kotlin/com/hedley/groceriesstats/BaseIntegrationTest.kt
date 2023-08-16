92c47036c306d7b0de7198024d115bae9e676898
Hedley Luna <hluna@stackstate.com>
2023-08-16 12:11:01 +0200

    Adding first real integration test

---
+++ src/test/kotlin/com/hedley/groceriesstats/BaseIntegrationTest.kt
@@ -0,0 +1,46 @@
+package com.hedley.groceriesstats
+
+import org.junit.jupiter.api.BeforeAll
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.junit.jupiter.Container
+
+open class BaseIntegrationTest {
+    companion object {
+        @Container
+        val postgreSQLContainer: PostgreSQLContainer<*> = PostgreSQLContainer("postgres:bullseye")
+            .withDatabaseName("integration-tests-db")
+
+        @DynamicPropertySource
+        @JvmStatic
+        fun registerDynamicProperties(registry: DynamicPropertyRegistry) {
+            registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl)
+            registry.add("spring.datasource.username", postgreSQLContainer::getUsername)
+            registry.add("spring.datasource.password", postgreSQLContainer::getPassword)
+
+            registry.add("spring.r2dbc.url", this::r2dbcUrl)
+            registry.add("spring.r2dbc.username", postgreSQLContainer::getUsername)
+            registry.add("spring.r2dbc.password", postgreSQLContainer::getPassword)
+
+            registry.add("spring.flyway.url", postgreSQLContainer::getJdbcUrl)
+            registry.add("spring.flyway.user", postgreSQLContainer::getUsername)
+            registry.add("spring.flyway.password", postgreSQLContainer::getPassword)
+
+        }
+
+        private fun r2dbcUrl(): String {
+            return "r2dbc:postgresql://${postgreSQLContainer.host}:${
+                postgreSQLContainer.getMappedPort(
+                    PostgreSQLContainer.POSTGRESQL_PORT
+                )
+            }/${postgreSQLContainer.databaseName}"
+        }
+
+        @JvmStatic
+        @BeforeAll
+        internal fun setUp(): Unit {
+            postgreSQLContainer.start()
+        }
+    }
+}
--- src/test/kotlin/com/hedley/groceriesstats/DemoTest.kt
+++ src/test/kotlin/com/hedley/groceriesstats/DemoTest.kt
@@ -2,68 +2,40 @@ package com.hedley.groceriesstats
 
 import com.hedley.groceriesstats.franchises.Franchise
 import com.hedley.groceriesstats.franchises.FranchiseRepository
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.DynamicPropertyRegistry
-import org.springframework.test.context.DynamicPropertySource
-import org.testcontainers.containers.PostgreSQLContainer
-import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 
 @SpringBootTest
-//@DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Testcontainers
-class DemoTestTests {
-
-    companion object {
-        @Container
-        val postgreSQLContainer: PostgreSQLContainer<*> = PostgreSQLContainer("postgres:bullseye")
-            .withDatabaseName("integration-tests-db")
-
-        @DynamicPropertySource
-        @JvmStatic
-        fun registerDynamicProperties(registry: DynamicPropertyRegistry) {
-            registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl)
-            registry.add("spring.datasource.username", postgreSQLContainer::getUsername)
-            registry.add("spring.datasource.password", postgreSQLContainer::getPassword)
-
-            registry.add("spring.r2dbc.url", Companion::r2dbcUrl)
-            registry.add("spring.r2dbc.username", postgreSQLContainer::getUsername)
-            registry.add("spring.r2dbc.password", postgreSQLContainer::getPassword)
-
-            registry.add("spring.flyway.url", postgreSQLContainer::getJdbcUrl)
-            registry.add("spring.flyway.user", postgreSQLContainer::getUsername)
-            registry.add("spring.flyway.password", postgreSQLContainer::getPassword)
-
-        }
-
-        fun r2dbcUrl(): String {
-            return "r2dbc:postgresql://${postgreSQLContainer.host}:${
-                postgreSQLContainer.getMappedPort(
-                    PostgreSQLContainer.POSTGRESQL_PORT
-                )
-            }/${postgreSQLContainer.databaseName}"
-        }
-
-        @JvmStatic
-        @BeforeAll
-        internal fun setUp(): Unit {
-            postgreSQLContainer.start()
-        }
-    }
+class FranchiseRepositoryTest : BaseIntegrationTest() {
 
     @Autowired
     private lateinit var franchiseRepository: FranchiseRepository
+
+    @AfterEach
+    fun clear() {
+        franchiseRepository.deleteAll()
+    }
 
     @Test
     fun `it loads a franchise by id`() {
         val saved = franchiseRepository.save(Franchise(id = null, name = "Test")).block()
         val found = franchiseRepository.findById(saved!!.id!!).block()
         Assertions.assertEquals(saved, found)
+    }
+
+    @Test
+    fun `it finds by name ignoring the case`() {
+        val saved = franchiseRepository.save(Franchise(id = null, name = "Testing")).block()
+        val lowercase = franchiseRepository.findByNameContainingIgnoreCase("test").collectList().block()
+        val uppercase = franchiseRepository.findByNameContainingIgnoreCase("Test").collectList().block()
+        Assertions.assertEquals(saved, lowercase!![0])
+        Assertions.assertEquals(saved, uppercase!![0])
     }
 }
